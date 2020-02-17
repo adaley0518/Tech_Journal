8ce105176bbea0fff6cd2d8475013c0e2c06c740
adaley0518 <adaley0518@gmail.com>
2020-02-17 11:24:25 -0500

    add file

---
+++ SYS265/linux/centos7/secure-ssh.sh
@@ -0,0 +1,6 @@
+#secure-ssh.sh
+#author adaley0518
+#creates a new ssh user using $l parameter
+#adds a public key from the local repos or curled from the remote repo
+#removes roots ability to ssh in 
+echo "ALL YOUR CODE GOES HERE"
